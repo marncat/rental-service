4346e161c473959dc93a2967c8de1d9440ecb4a3
float-me <main.float@proton.me>
2025-05-13 12:13:30 +0900

    admin page layout change

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -97,16 +97,23 @@
 	});
 </script>
 
-<h1>관리자 - 품목 추가</h1>
-<form action="/admin/logout" method="POST" use:enhance>
-	<button type="submit">로그아웃</button>
-</form>
+<div
+	style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 1rem;"
+>
+	<h1>관리자</h1>
+	<form action="/admin/logout" method="POST" use:enhance>
+		<button type="submit">로그아웃</button>
+	</form>
+</div>
 
-<form onsubmit={addItem} style="margin-bottom: 2rem;">
-	품목 이름: <input bind:value={form.name} required />
-	분류: <input bind:value={form.category} required />
-	<button type="submit">추가</button>
-</form>
+<h2>📋 품목 추가</h2>
+<div style="border: 1px solid #ccc; padding: 1rem; margin-bottom: 2rem; ">
+	<form onsubmit={addItem}>
+		품목 이름: <input bind:value={form.name} required />
+		분류: <input bind:value={form.category} required />
+		<button type="submit">추가</button>
+	</form>
+</div>
 
 <h2>📋 전체 품목 목록</h2>
 
